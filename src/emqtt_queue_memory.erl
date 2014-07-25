5fe065b1aad6c977988f427d531660b4d0419816
Álvaro Pagliari <alvaropag@gmail.com>
2014-07-25 02:34:33 -0300

    Queue compiling and an abstraction developed in emqtt_queue

--- src/emqtt_queue_memory.erl
+++ src/emqtt_queue_memory.erl
@@ -2,40 +2,31 @@
 
 -include("emqtt_queue.hrl").
 
-%% new(id :: binary()) - {ok, queue_record} | {error, Reason}
-%% size(queue_record) - {ok, ::integer()} | {error, Reason} 
-%% id() - {ok, binary()} - returns the client_id associated
-%% in(queue_item, Item) - ok | {error, Reason}
-%% out(queue_record) - {ok, Item} | {error, Reason}
-%% peek(queue_record, N) - {ok, [Item]} | {error, Reason} - return the first element of the list without removing it
-%% drop_queue(queue_record) - ok | {error, Reason}
-%% member_of(queue_record, Item) - ok | {error, Reason}
-
 -export([
          new/1,
          id/1,
          in/2,
-         out/2,
+         out/1,
          peek/2,
          member_of/2
-         ]}.
+         ]).
          
 
 new(Queue_id) when is_binary(Queue_id) ->
     Queue = queue:new(),
-    {ok, #emqtt_queue{queue_id = Queue_id, queue_ref = Queue, queue_cb = emqtt_queue_memory}};
+    {ok, #emqtt_queue{queue_id = Queue_id, queue_ref = Queue, size = 0}};
 
 new(_) ->
     {error, "Queue_id must be binary"}.
 
 
-id(#emqtt_queue{queue_id = QueueId} = Queue) ->
+id(#emqtt_queue{queue_id = QueueId}) ->
     QueueId;
 id(_) ->
     {error, "Invalid queue handler"}.
 
 
-in(#emqtt_queue{queue_ref = QueueRef, size = Size} = Queue, Item) when is_queue(QueueRef)->
+in(#emqtt_queue{queue_ref = QueueRef, size = Size} = Queue, Item) ->
     QueueRef2 = queue:in(Item, QueueRef),
     {ok, Queue#emqtt_queue{queue_ref = QueueRef2, size = Size + 1}};
 
@@ -43,10 +34,10 @@ in(_, _) ->
     {error, "Invalid queue handler"}.
 
 
-out(#emqtt_queue{queue_ref = QueueRef, size = 0} = Queue) when is_queue(QueueRef)  ->
+out(#emqtt_queue{size = 0} = Queue) ->
     {ok, {empty}, Queue};
 
-out(#emqtt_queue{queue_ref = QueueRef, size = Size} = Queue) when is_queue(QueueRef)  ->
+out(#emqtt_queue{queue_ref = QueueRef, size = Size} = Queue) ->
     {{value, Item}, QueueRef2}  = queue:out(QueueRef),
     {ok, {value, Item}, Queue#emqtt_queue{queue_ref = QueueRef2, size = Size -1}};
 
@@ -54,24 +45,24 @@ out(_) ->
     {error, "Invalid queue handler"}.
 
 
-peek(#emqtt_queue{queue_ref = QueueRef, size = Size} = Queue, N) when is_queue(QueueRef), Size = 0 ->
+peek(#emqtt_queue{size = Size} = Queue, _) when Size == 0 ->
     {ok, {empty}, Queue};
 
-peek(#emqtt_queue{queue_ref = QueueRef, size = Size} = Queue, N) when is_queue(QueueRef); Size <= 0 ; N <= 0 ->
+peek(#emqtt_queue{size = Size}, N) when Size < 0; is_integer(N); N =< 0 ->
     {error, "Invalid parameters"};
 
-peek(#emqtt_queue{queue_ref = QueueRef, size = Size} = Queue, N) when is_queue(QueueRef), Size > 0, N > 0, N <= Size ->
-    {Queue2, Queue3} = queue:split(N, QueueRef),
+peek(#emqtt_queue{queue_ref = QueueRef, size = Size} = Queue, N) when Size > 0, is_integer(N), N > 0, N =< Size ->
+    {Queue2, _} = queue:split(N, QueueRef),
     {ok, {value, queue:to_list(Queue2)}, Queue};
 
 peek(_, _) ->
     {error, "Invalid queue handler"}.
 
 
-member_of(#emqtt_queue(queue = QueueRef) = Queue, Item) when is_queue(QueueRef), is_empty(QueueRef) ->
+member_of(#emqtt_queue{size = 0} = Queue, _) ->
     {ok, {empty}, Queue};
 
-member_of(#emqtt_queue(queue = QueueRef) = Queue, Item) when is_queue(QueueRef) ->
+member_of(#emqtt_queue{queue_ref = QueueRef} = Queue, Item) ->
     {ok, {queue:member(Item, QueueRef)}, Queue};
 
 member_of(_, _) ->
